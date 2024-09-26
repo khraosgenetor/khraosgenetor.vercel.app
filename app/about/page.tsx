79071939c5c3940f60f53bbc550a951b1d25e8c6
Khraos Genetor <khraos.genetor@gmail.com>
2024-09-26 06:21:07 +0530

    Added a custom scroll bar

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
     description: "About Me",
 };
 
-export default function Home() {
+export default function About() {
   return (
     <RootLayout>
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -47,53 +47,49 @@ export default function Home() {
                         />
                     </div>
                 </div>
-                <div className="flex justify-center items-center w-full gap-2">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#1b1b1b] text-[#f5f5f5] gap-2 hover:bg-[#555555] dark:hover:bg-[#121212] hover:text-gray-500 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://github.com/khraosgenetor/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark"
-                            src="/svgs/icons8-github.svg"
-                            alt="Vercel logomark"
-                            width={35}
-                            height={35}
+                <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+                    <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+                        Programming languages I know:
+                            <ol>
+                                <li>Java</li>
+                                <li>C++</li>
+                                <li>C</li>
+                                <li>GoLang</li>
+                                <li>Rust</li>
+                                <li>JS and TypeScript</li>
+                            </ol>
+                    </div>
+                    <hr className="w-12 h-[2px] bg-gray-500 self-center" style={{ width: "1px", height: "200px", display: "inline-block" }}/>
+                    <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+                        Tools I use:
+                        <ol>
+                            <li>Git (GitHub)</li>
+                            <li>Vercel</li>
+                            <li>VS Code</li>
+                            <li>JetBrains IDEs</li>
+                            <li>BlueJ</li>
+                            <li>Docker</li>
+                            <li>NodeJS</li>
+                            <li>Vim and NeoVim</li>
+                        </ol>
+                    </div>
+                </div>
+                <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+                    <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+                        Listen to this playlist, it has a lot of phonk.
+                    </div>
+                    <hr className="w-12 h-[2px] bg-gray-500 self-center" style={{ width: "1px", height: "200px", display: "inline-block" }}/>
+                    <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-co">
+                    <iframe
+                            style={{ borderRadius: '12px', width: '100%', height: '352px' }}
+                            src="https://open.spotify.com/embed/playlist/6a58Nak539mbZWk8Prq33k?utm_source=generator"
+                            frameBorder="0"
+                            allowFullScreen
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"
                         />
-                        @khraosgenetor
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#1b1b1b] text-[#f5f5f5] gap-2 hover:bg-[#555555] dark:hover:bg-[#121212] hover:text-gray-500 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://instagram.com/khraosgenetor/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark"
-                            src="/svgs/icons8-instagram.svg"
-                            alt="Vercel logomark"
-                            width={35}
-                            height={35}
-                        />
-                        @khraosgenetor
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#1b1b1b] text-[#f5f5f5] gap-2 hover:bg-[#555555] dark:hover:bg-[#121212] hover:text-gray-500 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                        href="https://discord.com/users/1219542034998628352"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark"
-                            src="/svgs/icons8-discord.svg"
-                            alt="Vercel logomark"
-                            width={35}
-                            height={35}
-                        />
-                        @khraos.genetor_
-                    </a>
-            </div>
+                    </div>
+                </div>
             </main>
             <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
                 <a
