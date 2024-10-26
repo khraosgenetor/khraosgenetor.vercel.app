481d09b233b4b799d545918e9b38584c76c20064
Khraos Genetor <khraos.genetor@gmail.com>
2024-10-26 18:25:27 +0530

    Fixed a url typo from github_repos to github-repos

--- app/RefTSX/Header.tsx
+++ app/RefTSX/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
             </a>
             <a
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#1b1b1b] text-[#f5f5f5] gap-2 hover:bg-[#555555] dark:hover:bg-[#121212] hover:text-gray-500 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                href="/github_repos"
+                href="/github-repos"
             >
                 GitHub Repos
             </a>
