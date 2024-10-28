57a40cb2ff91ee0c297d9d8d23077c8b99f3c967
Khraos Genetor <khraos.genetor@gmail.com>
2024-10-28 17:05:23 +0530

    Formatted the code

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,39 +1,42 @@
 "use client";
 
-import { usePathname } from 'next/navigation'
-import Header from './RefTSX/Header'
-import Footer from './RefTSX/Footer'
-import Image from 'next/image';
+import { usePathname } from "next/navigation";
+import Header from "./RefTSX/Header";
+import Footer from "./RefTSX/Footer";
+import Image from "next/image";
 
 export default function NotFound() {
   const pathname = usePathname();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <Header />
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
-                <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col text-center">
-                    <ul className="list-none">
-                        <li>404 - The path "{pathname}" does not exist, yet.</li>
-                        <li>Contact KhraosGenetor if the site breaks.</li>
-                    </ul>
-                </div>
+      <Header />
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+          <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col text-center">
+            <ul className="list-none">
+              <li>404 - The path "{pathname}" does not exist, yet.</li>
+              <li>Contact KhraosGenetor if the site breaks.</li>
+            </ul>
+          </div>
 
-                <hr className="w-12 h-[2px] bg-gray-500 self-center" style={{ width: "1px", height: "200px", display: "inline-block" }}/>
-                <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
-                    <Image
-                        className="dark"
-                        src="/svgs/404-error.svg"
-                        alt="404 Error"
-                        width={300}
-                        height={300}
-                        priority
-                    />
-                </div>
-            </div>
-    </main>
-    <Footer />
+          <hr
+            className="w-12 h-[2px] bg-gray-500 self-center"
+            style={{ width: "1px", height: "200px", display: "inline-block" }}
+          />
+          <div className="flex justify-center items-center w-full gap-8 sm:flex-row flex-col">
+            <Image
+              className="dark"
+              src="/svgs/404-error.svg"
+              alt="404 Error"
+              width={300}
+              height={300}
+              priority
+            />
+          </div>
+        </div>
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,27 @@ import Footer from "./RefTSX/Footer";
 export default function Home() {
   return (
     <RootLayout>
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Header />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <Header />
 
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <div className="flex justify-center items-center w-full">
+          <div className="flex justify-center items-center w-full">
             <Image
-                className="dark"
-                src="/personal/pfp.png"
-                alt="Next.js logo"
-                width={180}
-                height={38}
-                priority
+              className="dark"
+              src="/personal/pfp.png"
+              alt="Next.js logo"
+              width={180}
+              height={38}
+              priority
             />
-            </div>
-            <div className="flex justify-center items-center w-full text-center">
-                Welcome to my website. My name, is Khraos Genetor.<br></br>
-                Hope you have a good day here.
-            </div>
+          </div>
+          <div className="flex justify-center items-center w-full text-center">
+            Welcome to my website. My name, is Khraos Genetor.<br></br>
+            Hope you have a good day here.
+          </div>
         </main>
-            <Footer />
-        </div>
+        <Footer />
+      </div>
     </RootLayout>
   );
 }
