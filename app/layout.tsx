69c219ab994e9bbc33b4ad28f7632541bb526695
Khraos Genetor <khraos.genetor@gmail.com>
2025-01-13 14:59:30 +0530

    Imported React from React

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import React from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
