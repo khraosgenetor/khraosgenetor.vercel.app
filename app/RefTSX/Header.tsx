8b0dfe6b12fefe8f9c91719e32959e8bff728642
Khraos Genetor (Aditya Abhiram) <khraos.genetor@gmail.com>
2025-03-27 12:51:03 +0530

    Simplified redirects

--- app/RefTSX/Header.tsx
+++ app/RefTSX/Header.tsx
@@ -40,7 +40,7 @@ export default function Header({ className }: HeaderProps) {
 
             <a
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[#1b1b1b] text-[#f5f5f5] gap-2 hover:bg-[#555555] dark:hover:bg-[#121212] hover:text-gray-500 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-                href="https://portfolio.khraos.in"
+                href="/portfolio"
             >
                 Portfolio
             </a>
